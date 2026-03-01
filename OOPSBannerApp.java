3f665f4f462df72c272b5b61be985b4f2f74c6b1
vp7203-boop <vp7203@srmist.edu.in>
2026-03-01 22:37:16 +0530

    third usecase

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,13 +1,14 @@
+
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        System.out.println("  ***     " + "  ***     " + " *****   " + " ***** ");
-        System.out.println(" ** **    " + " ** **    " + " **  **  " + " **    ");
-        System.out.println("**   **   " + "**   **   " + " **   ** " + " **    ");
-        System.out.println("**   **   " + "**   **   " + " *****   " + " ***   ");
-        System.out.println("**   **   " + "**   **   " + " **      " + "   **  ");
-        System.out.println(" ** **    " + " ** **    " + " **      " + "   **  ");
-        System.out.println("  ***     " + "  ***     " + " **      " + " ***** ");
+        System.out.println(String.join(" ", "  ***     ", "  ***     ", " *****   ", " ***** "));
+        System.out.println(String.join(" ", " ** **    ", " ** **    ", " **  **  ", " **    "));
+        System.out.println(String.join(" ", "**   **   ", "**   **   ", " **   ** ", " **    "));
+        System.out.println(String.join(" ", "**   **   ", "**   **   ", " *****   ", " ***   "));
+        System.out.println(String.join(" ", "**   **   ", "**   **   ", " **      ", "   **  "));
+        System.out.println(String.join(" ", " ** **    ", " ** **    ", " **      ", "   **  "));
+        System.out.println(String.join(" ", "  ***     ", "  ***     ", " **      ", " ***** "));
     }
 }
