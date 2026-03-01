6c63f4682dd407f27890ab054bb3a3f8cb0a4837
vp7203-boop <vp7203@srmist.edu.in>
2026-03-01 23:36:39 +0530

    sixth usercase

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,21 +1,64 @@
 
-
 public class OOPSBannerApp {
 
+    // Pads a string to a fixed width with spaces on the right
+    public static String pad(String s, int width) {
+        return String.format("%-" + width + "s", s);
+    }
+
+    // Method for letter 'O' - fixed width: 7
+    public static String[] getOPattern() {
+        return new String[] {
+            "  ***  ",
+            " ** ** ",
+            "**   **",
+            "**   **",
+            "**   **",
+            " ** ** ",
+            "  ***  "
+        };
+    }
+
+    // Method for letter 'P' - fixed width: 7
+    public static String[] getPPattern() {
+        return new String[] {
+            "****** ",
+            "**  ** ",
+            "**  ** ",
+            "****** ",
+            "**     ",
+            "**     ",
+            "**     "
+        };
+    }
+
+    // Method for letter 'S' - fixed width: 7
+    public static String[] getSPattern() {
+        return new String[] {
+            "***** ",
+            "**     ",
+            "**     ",
+            " ***   ",
+            "   **  ",
+            "   **  ",
+            "***** "
+        };
+    }
+
     public static void main(String[] args) {
+        String[] oPattern = getOPattern();
+        String[] pPattern = getPPattern();
+        String[] sPattern = getSPattern();
 
-        String[] lines = new String[7];
+        int width = 7;
 
-        lines[0] = String.join(" ", "  ***     ", "  ***     ", " *****   ", " ***** ");
-        lines[1] = String.join(" ", " ** **    ", " ** **    ", " **  **  ", " **    ");
-        lines[2] = String.join(" ", "**   **   ", "**   **   ", " **   ** ", " **    ");
-        lines[3] = String.join(" ", "**   **   ", "**   **   ", " *****   ", " ***   ");
-        lines[4] = String.join(" ", "**   **   ", "**   **   ", " **      ", "   **  ");
-        lines[5] = String.join(" ", " ** **    ", " ** **    ", " **      ", "   **  ");
-        lines[6] = String.join(" ", "  ***     ", "  ***     ", " **      ", " ***** ");
-
-        for (String line : lines) {
-            System.out.println(line);
+        for (int i = 0; i < oPattern.length; i++) {
+            System.out.println(
+                pad(oPattern[i], width) + "  " +
+                pad(oPattern[i], width) + "  " +
+                pad(pPattern[i], width) + "  " +
+                pad(sPattern[i], width)
+            );
         }
     }
 }
