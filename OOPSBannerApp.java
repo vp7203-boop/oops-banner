88c70f0c4001957a59a3ed77641cf23bda58f12c
vp7203-boop <vp7203@srmist.edu.in>
2026-03-01 20:31:29 +0530

    second usecase

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -2,6 +2,12 @@ public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        System.out.println("OOPS");
+        System.out.println("  ***     " + "  ***     " + " *****   " + " ***** ");
+        System.out.println(" ** **    " + " ** **    " + " **  **  " + " **    ");
+        System.out.println("**   **   " + "**   **   " + " **   ** " + " **    ");
+        System.out.println("**   **   " + "**   **   " + " *****   " + " ***   ");
+        System.out.println("**   **   " + "**   **   " + " **      " + "   **  ");
+        System.out.println(" ** **    " + " ** **    " + " **      " + "   **  ");
+        System.out.println("  ***     " + "  ***     " + " **      " + " ***** ");
     }
 }
